de9dbde5c72a06c937029808108d2223ebece297
mnatty <men6288@rit.edu>
2017-05-10 15:02:54 -0400

    made the footer sticky, widened the form fields

--- assets/includes/footer.php
+++ assets/includes/footer.php
@@ -1,9 +1,10 @@
 <!--
 This include is for the footer on each page
 -->
-<footer>
-    <div class="footer-fixed">
+<div class="footer">
+    <div class="footer-style">
         <p>&copy; 2017 ISTE-330 Project</p>
         <p>Prepared by Ricky Catron, Clayton Brimm, Calvin Gonzalez, and Matthew Nataloni</p>
     </div>
-</footer>
+</div>
+
